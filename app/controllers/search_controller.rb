1ee4ea9406a10804cb4ab4ff4676a1dc8a236b85
Josh Mejia <joshua.mejia@gmail.com>
2017-10-10 11:06:30 -0600

    Start to implement API call.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,12 @@
 class SearchController < ApplicationController
   def index
-    
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+    conn.headers["X-API-Key"] = "yQGAnrKoJi7WWVfZ4LsbCINgYlhrXrm9YrpF2zE8"
+    response = conn.get("/congress/v1/members/house/CO/current.json")
+    @members = JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
